7340a3241480a88015af84a9b9e4cd4542c2b4bb
RUBESH <rubeshkriz@gmail.com>
2020-09-09 22:16:43 +0530

    3_basic state Setup

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,10 +1,16 @@
 import React, { Component } from "react";
+import EventDashboard from "../../features/event/EventDashboard/EventDashboard";
+import NavBar from "../../features/nav/NavBar/NavBar";
+import { Container } from "semantic-ui-react";
 
 class App extends Component {
   render() {
     return (
       <div className='App'>
-        <h1>lovelin</h1>
+        <NavBar />
+        <Container className='main'>
+          <EventDashboard />
+        </Container>
       </div>
     );
   }
---
+++ src/features/event/EventList/EventListAttendee.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import { ListItem, Image } from "semantic-ui-react";
+
+class EventListAttendee extends Component {
+  render() {
+    const { attendee } = this.props;
+    return (
+      <ListItem>
+        <Image as='a' size='mini' circular src={attendee.photoURL} />
+      </ListItem>
+    );
+  }
+}
+
+export default EventListAttendee;
